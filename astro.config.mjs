4a3cfe5fe07b7e9b1b48ba791ed12093c0f15c65
Ashfid <ashfidable.islam@gmail.com>
2023-02-01 19:32:51 +0300

    astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
   integrations: [tailwind(), svelte()]
 });
