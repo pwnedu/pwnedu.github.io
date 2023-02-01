f170574209a5586fbf557c24de2e827979387b2b
Ashfid <ashfidable.islam@gmail.com>
2023-02-01 04:14:23 +0300

    fix more fetches

--- src/helpers/fetchGithubProjects.ts
+++ src/helpers/fetchGithubProjects.ts
@@ -1,6 +1,6 @@
 export default async function getAllGithubRepos() {
     const repos = []
-    let result = await fetch("https://gist.githubusercontent.com/ashfidable/415e0609fe4ff751f2a95ba13d509846/raw/658d761276575d95c9a3535b1a7ac96c7eb6a07c/list-all-repo.json").then(
+    let result = await fetch("https://api.github.com/users/pwnedu/repos").then(
         (res) => res.json()
     );
     result = result.sort(
