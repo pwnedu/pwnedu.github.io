e512667fc13e99298ade9e971d981b6a23ef0525
Ashfid <ashfidable.islam@gmail.com>
2023-04-13 04:06:25 +0300

    chore: added mdx support for snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://astronaut.github.io',
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), mdx()]
 });
