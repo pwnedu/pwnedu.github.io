c0cedae9b830feaa5603ac11683e431cc9e0b4d2
Ashfid <ashfidable.islam@gmail.com>
2024-01-12 14:24:33 +0300

    style: added new variables and style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,34 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: 'hsl(var(--primary-color) / <alpha-value> )',
-				secondary: 'hsl(var(--secondary-color) / <alpha-value> )',
-				accent: 'hsl(var(--accent-color) / <alpha-value> )',
-				'accent-invert': 'hsl(var(--accent-invert) / <alpha-value> )'
-			},
-			boxShadow: {
-				'solid-1': '0 0 0 1pt black',
-				'solid-2': '0 0 0 2pt black',
-
-			},
-			backgroundColor: {
-				main: 'hsl(var(--bg-color) / <alpha-value> )',
-
-			},
-			typography: ({ theme }) => ({
-				test: {
-					css: {
-						'--tw-prose-body': 'hsl(var(--accent-color) / <alpha-value> )',
-						'--tw-prose-headings': 'hsl(var(--accent-color) / <alpha-value> )',
-					}
-				}
-			})
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary-color) / <alpha-value> )",
+        secondary: "hsl(var(--secondary-color) / <alpha-value> )",
+        accent: "hsl(var(--accent-color) / <alpha-value> )",
+        "accent-invert": "hsl(var(--accent-invert) / <alpha-value> )",
+        "header-snippet": "var(--header-snippet-color)",
+      },
+      boxShadow: {
+        "solid-1": "0 0 0 1pt black",
+        "solid-2": "0 0 0 2pt black",
+      },
+      backgroundColor: {
+        main: "hsl(var(--bg-color) / <alpha-value> )",
+      },
+      typography: ({ theme }) => ({
+        test: {
+          css: {
+            "--tw-prose-body": "hsl(var(--accent-color) / <alpha-value> )",
+            "--tw-prose-headings": "hsl(var(--accent-color) / <alpha-value> )",
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
